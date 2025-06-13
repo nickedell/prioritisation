1469b73e73d6182567454902580971bc2dbb98d9
Nick Edell <various@humble2.com>
2025-06-13 21:54:48 +0100

    b

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
+import './index.css'; // <-- ADD THIS LINE
 
-// This finds the <div id="root"> in your index.html...
-const rootElement = document.getElementById('root')!;
-
-// ...and tells React to render your <App /> component inside it.
-ReactDOM.createRoot(rootElement).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
